d0238502ed40622d04d0fbd2ec5d51a9090010d2
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-02-10 14:32:29 -0500

    Added comments in binary_to_asm and added 2 preprocessor directives in .c file

--- inc/binary_to_asm.h
+++ inc/binary_to_asm.h
@@ -1,4 +1,12 @@
-#ifndef BINARY_TO_ASM_H
+/*
+ * FILE: binary_to_asm.h
+ * PROJECT: encodeInput Utility
+ * PROGRAMMER: MANREET THIND (STUDENT ID: 8982315)
+ * FIRST VERSION: 02/10/2025
+ * DESCRIPTION:
+ * This header file declares functions for encoding binary input into an assembly file format.
+ */
+ #ifndef BINARY_TO_ASM_H
 #define BINARY_TO_ASM_H
 
 #include <stdio.h>
--- src/binary_to_asm.c
+++ src/binary_to_asm.c
@@ -1,5 +1,24 @@
+/*
+ * FILE: binary_to_asm.c
+ * PROJECT: encodeInput Utility
+ * PROGRAMMER: MANREET THIND (STUDENT ID: 8982315)
+ * FIRST VERSION: 01/26/2025
+ * DESCRIPTION:
+ * This file contains the implementation of a function that converts binary data 
+ * into an assembly-formatted string using the "DB" directive.
+ */
+#include <stdio.h>
+#include <stdlib.h>
 #include "../inc/binary_to_asm.h"
 
+/*
+ * Name    : convert_binary_to_asm
+ * Purpose : Converts binary data from an input file into assembly format and writes to an output file.
+ * Input   : input  - FILE* - Pointer to the binary input file
+ *           output - FILE* - Pointer to the assembly output file
+ * Outputs : Writes assembly-formatted data to the output file.
+ * Returns : void
+ */
 void convert_binary_to_asm(FILE *input, FILE *output) {
     if (!input || !output) {  // Check for valid file pointers
         fprintf(stderr, "Error: Invalid file pointers provided.\n");
