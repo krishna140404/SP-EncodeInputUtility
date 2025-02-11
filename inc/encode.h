b05f58e28ff88e3dee9c6be2c914654ff05db41b
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2025-02-11 16:57:53 -0500

    Created the encode .c and .h file

---
+++ inc/encode.h
@@ -0,0 +1,8 @@
+/*
+ * FILE: encode.h
+ * PROJECT: encodeInput Utility
+ * PROGRAMMER: BHAWANJEET KAUR GILL (STUDENT ID: 8958829)
+ * FIRST VERSION: 02/11/2025
+ * DESCRIPTION:
+ * This header file declares the functions for encoding the functions altogether.
+ */
